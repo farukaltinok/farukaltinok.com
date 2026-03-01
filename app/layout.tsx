6c5033f9a4753df63c1add5f4ca40234a636fa61
Faruk Altinok <farukaltinok@Faruk-3.fritz.box>
2026-03-01 20:43:05 +0100

    deine Nachricht

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Cormorant_Garamond } from "next/font/google";
 import { LanguageProvider } from "./components/LanguageContext";
+import { Analytics } from "@vercel/analytics/react";
 
 const siteSerif = Cormorant_Garamond({
   subsets: ["latin"],
@@ -41,6 +42,7 @@ export default function RootLayout({
           </div>
           <Footer />
         </LanguageProvider>
+        <Analytics />
       </body>
     </html>
   );
